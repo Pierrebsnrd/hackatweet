b3b5cb5f80e4b8e40e3c4d2193772c9044a9c48d
Pierre Boisnard <pierre.boisnard@live.fr>
2025-02-13 13:14:11 +0100

    udpate modal signup

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import SignIn from './SignIn'
 import SignUp from './SignUp'
-import Image from "next/Image";
+import Image from "next/image";
 import styles from '../styles/Login.module.css'
 
 function Login() {
   return (
     <div className={styles.login}>
       <Image src='/gobelin.jpg' alt="gobelin" width={100} height={100} />
-      <h1>See what's happening</h1>
       <div className={styles.modale}>
-      <SignIn/>
+      <h1>See what's happening</h1>
+      <h2>Join Hackatweet today</h2>
       <SignUp/>
+      <h3>Already have an account ?</h3>
+      <SignIn/>
       </div>
     </div>
   )
--- frontend/components/SignUp.js
+++ frontend/components/SignUp.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Modal, Button } from "antd";
+import Image from "next/image";
+import styles from '../styles/Login.module.css'
 
 function SignUp() {
+  const { createRoot } = ReactDOM;
+  const { useState } = React;
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div>SignUp</div>
-  )
+    <>
+      <Button type="primary" onClick={showModal}>
+        Sign Up
+      </Button>
+      <Modal open={isModalOpen} onOk={handleOk} okText='SignUp' onCancel={handleCancel} >
+          <Image src="/gobelin.jpg" alt="gobelin" width={100} height={100} />
+        <div className={styles.popup}>
+          <h2>Create your Hackatweet account</h2>
+          <input
+            //onChange={(e) => setUser(e.target.value)}
+            //value={user.username}
+            type="text"
+            id="firstname"
+            placeholder="firstname"
+          />
+          <input
+            //onChange={(e) => setUser(e.target.value)}
+            //value={user.username}
+            type="text"
+            id="username"
+            placeholder="username"
+          />
+          <input
+            //onChange={(e) => setUser(e.target.value)}
+            //value={user.username}
+            type="text"
+            id="password"
+            placeholder="password"
+          />
+        </div>
+      </Modal>
+    </>
+  );
 }
 
-export default SignUp
+export default SignUp;
