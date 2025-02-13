cc77e3f1e84ddc6b834bd3f4b22768bb7b1ad18f
Maxime Benard <maxime.benard.maxime@gmail.com>
2025-02-13 16:45:52 +0100

    component tweet

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -5,20 +5,40 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import user from '../reducers/user'
 
+
+
+import {persistStore, persistReducer} from 'redux-persist'
+import {PersistGate} from 'redux-persist/integration/react'
+import storage from 'redux-persist/lib/storage'
+
+// const reducers = combineReducers({user})
+const persistConfig = {key: 'hackatweet', storage };
+
 const store = configureStore({
- reducer: {user},
+  // reducer: persistReducer(persistConfig, reducers),
+  // middleware : (getDefaultMiddleware) => getDefaultMiddleware ({serializableCheck:false})
+  reducer: {user} // à virer lors du decomment
 });
 
-function App({ Component, pageProps }) {
- return (
-   <Provider store={store}>
-     <Head>
-       <title>Best friends</title>
-     </Head>
-     <Component {...pageProps} />
-   </Provider>
- );
-}
+// const persistor =persistStore(store);
 
+function App({ Component, pageProps }) {
+  return (
+    <Provider store={store}>
+      {/* <PersistGate persistor={persistor}> /}
+        <Head>
+          <title>Hackatweet</title>
+        </Head>
+        <Header />
+      <Component {...pageProps} />
+      {/ </PersistGate> */}
+      <Head>
+          <title>Hackatweet</title>
+      </Head>
+        
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
 
 export default App;
