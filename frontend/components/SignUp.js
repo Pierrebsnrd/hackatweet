bbee10e2f83980e06be00a9bec69e2da439bf19b
Pierre Boisnard <pierre.boisnard@live.fr>
2025-02-13 13:21:00 +0100

    update modale

--- frontend/components/SignUp.js
+++ frontend/components/SignUp.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Modal, Button } from "antd";
 import Image from "next/image";
-import styles from '../styles/Login.module.css'
+import styles from "../styles/Login.module.css";
 
 function SignUp() {
   const { createRoot } = ReactDOM;
@@ -11,6 +11,7 @@ function SignUp() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const showModal = () => {
     setIsModalOpen(true);
+
   };
   const handleOk = () => {
     setIsModalOpen(false);
@@ -24,8 +25,14 @@ function SignUp() {
       <Button type="primary" onClick={showModal}>
         Sign Up
       </Button>
-      <Modal open={isModalOpen} onOk={handleOk} okText='SignUp' onCancel={handleCancel}>
-          <Image src="/gobelin.jpg" alt="gobelin" width={100} height={100} />
+      <Modal
+        onCancel={this.handleCancel}
+        /* visible={this.state.visible} */
+        open={isModalOpen}
+        onOk={handleOk}
+        okText="SignUp"
+      >
+        <Image src="/gobelin.jpg" alt="gobelin" width={100} height={100} />
         <div className={styles.popup}>
           <h2>Create your Hackatweet account</h2>
           <input
