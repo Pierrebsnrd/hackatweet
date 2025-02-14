7cd4b2c59bcdb15fb01e83253c19f4182c2c5c00
Pierre Boisnard <pierre.boisnard@live.fr>
2025-02-14 09:46:32 +0100

    update

--- backend/routes/tweets.js
+++ backend/routes/tweets.js
@@ -11,30 +11,20 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', (req, res) => {
-  /*if (!checkBody(req.body, ['firstname','username', 'password'])) {
+  if (!checkBody(req.body, ['message'])) {
     res.json({ result: false, error: 'Missing or empty fields' });
     return;
-  }*/
-
-  // Check if the user has not already been registered
-  Tweet.findOne({ username: req.body.username }).then(data => {
-    if (data === null) {
-      //const hash = bcrypt.hashSync(req.body.password, 10);
+  }
 
       const newTweet = new Tweet({
         message: req.body.message,
-        //user : variable.
+        user : req.body.user
         //date : req.body.date,
       });
 
       newTweet.save().then(newDoc => {
         res.json({result: true, messages : newDoc});
       });
-    } else {
-      // User already exists in database
-      res.json({ result: false, error: 'User already exists' });
-    }
-  });
-});
+}) 
 
 module.exports = router;
