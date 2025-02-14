e0b5ccbf211632bf86a1fa5b3f755e709b05d3cd
Pierre Boisnard <pierre.boisnard@live.fr>
2025-02-14 16:13:55 +0100

    update

--- frontend/components/LastTweets.js
+++ frontend/components/LastTweets.js
@@ -9,27 +9,27 @@ function LastTweets() {
   const tweet = useSelector((state) => state.tweet.value);
   console.log(tweet);
 
-  
-
   if (tweet.isPosted) {
     return (
       <div className={styles.lasttweets}>
-        <div className={styles.utilisateur}>
-          <Image src="/gobelin.jpg" alt="gobelin" width={100} height={100} />
-          <p className={styles.firstname}>{tweet.message.firstname}</p>
-          <p className={styles.post}>{tweet.message.username}</p>
-          <p className={styles.post}>timepost</p>
-        </div>
-        <div>
-          <p className={styles.msg}>{tweet.message.message}</p>
-        </div>
-        <div className={styles.icone}>
-          <FontAwesomeIcon
-            icon={faHeart} /* onClick={() => handleHiddenClick()} */
-          />
-          <FontAwesomeIcon
-            icon={faTrash} /* onClick={() => handleHiddenClick()} */
-          />
+        <div className={styles.oneTweet}>
+          <div className={styles.utilisateur}>
+            <Image src="/gobelin.jpg" alt="gobelin" width={100} height={100} />
+            <p className={styles.firstname}>{tweet.message.firstname}</p>
+            <p className={styles.post}>{tweet.message.username}</p>
+            <p className={styles.post}>timepost</p>
+          </div>
+          <div>
+            <p className={styles.msg}>{tweet.message.message}</p>
+          </div>
+          <div className={styles.icone}>
+            <FontAwesomeIcon
+              icon={faHeart} /* onClick={() => handleHiddenClick()} */
+            />
+            <FontAwesomeIcon
+              icon={faTrash} /* onClick={() => handleHiddenClick()} */
+            />
+          </div>
         </div>
       </div>
     );
